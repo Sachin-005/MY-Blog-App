97d274931504cab38f7fd5f100ec1e79e5d6f5ca
Sachin-005 <sachinsenthilvel2005@gmail.com>
2025-06-12 18:43:04 +0530

    complete sign in ui and api route

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,5 +1,109 @@
+import { Alert, Button, Label, TextInput, Spinner } from 'flowbite-react';
+import { useState } from 'react';
+import {Link, useNavigate} from  'react-router-dom';
 export default function SignIn() {
+  const [formData, setFormData] = useState({});
+  const [errorMessage, setErrorMessage] = useState('');
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,[e.target.id]: e.target.value});
+    }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setErrorMessage('');
+    setLoading(true);
+
+    if(!formData.email || !formData.password) {
+      return setErrorMessage('All fields are required');
+    } 
+    try {
+      const res = await fetch('/api/auth/signin', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+
+      const data = await res.json();
+
+      if(data.success == false) {
+        setLoading(false);
+        return setErrorMessage(data.message);
+      }
+    
+        setLoading(false);
+        console.log(data.message)
+        if(res.ok){
+          navigate('/');
+        }
+      
+    } catch (error) {
+      setLoading(false);
+      setErrorMessage(error.message);
+      console.log('Response:', res);
+
+    }
+  };
+
   return (
-    <div>SignIn</div>
-  )
+    <div className='min-h-screen mt-20'>
+      <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5'>
+        {/* left */}
+        <div className='flex-1'>
+          <Link to='/' className='font-bold dark:text-white text-4xl'>
+            <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>
+              Sachin's
+            </span>
+            Blog
+          </Link>
+         <p className='text-sm mt-5'>
+          Start your blogging journey with Sachin's Blog! Create an account in seconds 
+          using your email – no spam, just stories.
+        </p>
+        </div>
+        {/* right */}
+
+        <div className='flex-1'>
+          <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
+            <div>
+              <Label value='Your email' />
+              <TextInput type='email' placeholder='name@gmail.com' id='email'onChange={handleChange} />
+            </div>
+            <div>
+              <Label value='Your password' />
+              <TextInput type='password' placeholder='**********' id='password'onChange={handleChange} />
+            </div>
+            <Button gradientDuoTone='purpleToPink' type='submit' disabled={loading}>
+             
+              {loading ? (
+                <>
+                <Spinner size ='sm'/>
+                <span className='pl-3'>Loading...</span>
+                </>
+              ) : 'Sign In'}
+              
+              
+            </Button>
+          </form>
+          <div className="flex gap-2 text-sm mt-5">
+            <span>Don't have an account?</span>
+            <Link to='/sign-up' className='text-blue-500'>
+              Sign Up
+            </Link>
+          </div>
+          {
+            errorMessage && (
+              <Alert className='mt-5' color='failure'>
+                {errorMessage}
+              </Alert>
+            )
+          }
+        </div>
+      </div>
+    </div>
+  );
 }
