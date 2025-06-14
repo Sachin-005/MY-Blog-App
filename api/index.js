d0caa42f02f4965ad5c769d525975bfcd4a518e2
Sachin-005 <sachinsenthilvel2005@gmail.com>
2025-06-14 15:44:46 +0530

    add OAuth functionality

--- api/index.js
+++ api/index.js
@@ -1,36 +1,38 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import userRoutes from './routes/user.route.js';
-import authRoutes from './routes/auth.route.js';
-dotenv.config();
+    import express from 'express';
+    import mongoose from 'mongoose';
+    import dotenv from 'dotenv';
+    import userRoutes from './routes/user.route.js';
+    import authRoutes from './routes/auth.route.js';
+    dotenv.config();
 
-mongoose
-.connect(process.env.MONGODB_URI)
-.then(() => {
-    console.log('MongoDB connection established');
-}
-)
-.catch((err) => {       
-    console.error('MongoDB connection error:', err);
-});
+    mongoose
+    .connect(process.env.MONGODB_URI)
+    .then(() => {
+        console.log('MongoDB connection established');
+    }
+    )
+    .catch((err) => {       
+        console.error('MongoDB connection error:', err);
+    });
 
-const app = express();
-app.use(express.json());
+    const app = express();
+    app.use(express.json());
 
-app.listen(3000,()=>{
-    console.log('Server is running on port 3000');
-});
+    app.use('/api/user',userRoutes);
+    app.use('/api/auth',authRoutes);
 
-app.use('/api/user',userRoutes);
-app.use('/api/auth',authRoutes);
+   
+    // Error handling middleware
+    app.use((err,req,res,next) => {
+        const statusCode = err.statusCode || 500;
+        const message = err.message || 'Internal Server Error';
+        res.status(statusCode).json({
+            success: false,
+            statusCode,
+            message
+        })
+    });
 
-app.use((err,req,res,next) => {
-    const statusCode = err.statusCode || 500;
-    const message = err.message || 'Internal Server Error';
-    res.status(statusCode).json({
-        success: false,
-        statusCode,
-        message
-    })
-});
+    app.listen(3000,()=>{
+        console.log('Server is running on port 3000');
+    });
