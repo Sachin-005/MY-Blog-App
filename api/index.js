482e05cc977afcab7005781c32015f3541f52087
Sachin-005 <sachinsenthilvel2005@gmail.com>
2025-06-15 15:06:49 +0530

    added create post functionality

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@
     import dotenv from 'dotenv';
     import userRoutes from './routes/user.route.js';
     import authRoutes from './routes/auth.route.js';
+    import postRoutes from './routes/post.route.js';
     import cookieParser from 'cookie-parser';
     
     dotenv.config();
@@ -24,6 +25,7 @@
 
     app.use('/api/user',userRoutes);
     app.use('/api/auth',authRoutes);
+    app.use('/api/post',postRoutes);
 
    
     // Error handling middleware
